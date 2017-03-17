77d57ffe50e29551998db78b611ef836d92804ed
Madhu <madhu.muthumannan@gmail.com>
2017-03-17 10:57:29 +0530

    enabling pagination by adding https link

--- public/app/services/userService.js
+++ public/app/services/userService.js
@@ -13,10 +13,10 @@ angular.module('userService',[])
 				return $http.get('/api/users');
 			};	
 
-			// //api/users/
-			// userFactory.create=function(userData){
-			// 	return $http.post('/api/users/',userData);
-			// };
+			//api/users/
+			userFactory.create=function(userData){
+				return $http.post('/api/users/',userData);
+			};
 
 			//api/users/
 			userFactory.createNewUser=function(userData){
